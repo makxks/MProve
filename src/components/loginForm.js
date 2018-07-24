21bc46d5b4cc77b1dd09a28b9fa29c7ca9e4b156
Maximilian G. Platt <maxplatt7@gmail.com>
2018-07-24 18:32:19 +0800

    add tryingLogin state

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -10,7 +10,7 @@ class LoginForm extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { panelCssClass: "hidden", error: null };
+        this.state = { panelCssClass: "hidden", error: null, tryingLogin: false };
     }
 
     onSubmit(props) {
